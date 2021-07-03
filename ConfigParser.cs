a4036cad4764b7626b80a9fde0d3847ad2b9bce8
Alexcppio <62553154+Alexcppio@users.noreply.github.com>
2021-07-03 13:03:21 +0500

    Add files via upload

---
+++ ConfigParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class ConfigParser : IParser<Config>
+    {
+        private readonly string _str;
+        public ConfigParser(string s) => _str = s;
+        public Config Parse() => new Config(GetParameterValue(CreateLinesArr(), "path"), Int32.Parse(GetParameterValue(CreateLinesArr(), "number")));
+        private string[] CreateLinesArr() => _str.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        private string GetParameterValue(string[] lines, string paramName) => lines.FirstOrDefault(t => t.ToUpper().StartsWith(paramName)).ToString().Split('=')[1].Trim();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new ConfigParser(File.ReadAllText("config.txt"));
+            parser.Parse();
+        }
+    }
+
+}
