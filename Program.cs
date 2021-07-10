292966a8ea372d9d57dfde6643ab64f971599588
Alexcppio <62553154+Alexcppio@users.noreply.github.com>
2021-07-10 10:31:40 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp5
+namespace ConsoleApp6
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var parser = new ConfigParser(File.ReadAllText("config.txt"));
-            parser.Parse();
         }
-    }
+        //public (bool, Config, IEnumerable<Exception>) Parse()
+        //{
+        //    var exceptions = new List<Exception>();
+        //    string str;
+        //    try
+        //    {
+        //        var str = getParameterValue(createLinesArr(), "path");
+        //        throw new StackOverflowException();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        exceptions.Add(ex);
+        //    }
 
+        //    var str2 = getParameterValue(createLinesArr(), "number");
+        //    if (int.TryParse(str2, out var num))
+        //        return (true, new Config(str, num), null);
+        //    return (false, null, exceptions);
+        //}
+    }
 }
